f36a1dafe6f721b6fab27298e10795cf9469651d
Robert Yearling <laserguy@mac.com>
2021-01-12 14:48:37 -0600

    Final Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserForm from './components/UserForm';
-import FormResults from './components/FormResults';
 
 function App() {
 
-  const [state, setState] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    cfpassword:"",
-  });
-
   return (
     <div className="App">
-      <UserForm inputs={state} setInputs={setState}/>
-      <FormResults data={state} />
+      <UserForm></UserForm>
     </div>
   );
 }
--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,68 +1,84 @@
 import React, { useState } from 'react';
 
-const UserForm = props => {
+const UserForm = () => {
 
-    const [user, setUser] = useState({
+    const [userInfo, setUserInfo] = useState({
         firstName: "",
         lastName: "",
         email: "",
         password: "",
         cfpassword: "",
-    });
+    })
 
-    const handleChange = e => {
-        setUser({
-            ...user,
+    const [userList, setUserList] = useState([])
+
+    const handleChange = (e) => {
+        console.log("Value Entered", e.target.name)
+        setUserInfo({
+            ...userInfo,
             [e.target.name]: e.target.value
-        });
+        })
     }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setUserList([...userList, userInfo])
+        setUserInfo({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            cfpassword: "",
+        })
+    }
+
     return (
-        <form className="col-4 mx-auto text-left">
-            <h1>Create User:</h1>
-            <div className="form-group">
-                <label>First Name:</label>
-                <input
-                    type="text"
-                    name="firstName"
-                    className="form-control"
-                    onChange={handleChange}
-                    />
+        <>
+            <div>
+                <form onSubmit={submitHandler} className="col-4 mx-auto text-left">
+                    <h1>Create User:</h1>
+                    <div className="form-group">
+                        <label>First Name:</label>
+                        <input type="text" name="firstName" className="form-control" onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Last Name:</label>
+                        <input type="text" name="lastName" className="form-control" onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Email:</label>
+                        <input type="text" name="email" className="form-control" onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input type="text" name="password" className="form-control" onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Confirm Password:</label>
+                        <input type="text" name="cfpassword" className="form-control" onChange={handleChange} />
+                    </div>
+                    <div>
+                        <p><input type="submit" value="Add" /></p>
+                    </div>
+                </form>
             </div>
-            <div className="form-group">
-                <label>Last Name:</label>
-                <input type="text"
-                    name="lastName"
-                    className="form-control"
-                    onChange={handleChange}
-                    />
+            <div>
+                <h1>User Info:</h1>
+                {
+                    userList.map((user) => {
+                        return <div>
+                            <p>{user.firstName}</p>
+                            <p>{user.lastName}</p>
+                            <p>{user.email}</p>
+                            <p>{user.password}</p>
+                            <p>{user.cfpassword}</p>
+                            </div>
+                    })
+                }
             </div>
-            <div className="form-group">
-                <label>Email:</label>
-                <input type="text"
-                    name="email"
-                    className="form-control"
-                    onChange={handleChange}
-                    />
-            </div>
-            <div className="form-group">
-                <label>Password:</label>
-                <input
-                    type="text"
-                    name="password"
-                    className="form-control"
-                    onChange={handleChange}
-                    />
-            </div>
-            <div className="form-group">
-                <label>Confirm Password:</label>
-                <input
-                    type="text"
-                    name="cfpassword"
-                    className="form-control"
-                    onChange={handleChange}
-                    />
-            </div>
-        </form>
+
+        </>
+
     );
 }
 
